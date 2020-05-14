5c6f667c06fa5e8eabf364fc9cd8f6528b2303cc
Arod1207 <arod1207@me.com>
2020-05-13 23:02:12 -0500

    Local Storage Working For 1 button

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
-
-
+$(document).ready(function(){
 // Moment.js used to format Date and Time
 var currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
-
 $('#currentDay').html(currentDay);
+// Moments.js used to get Time ONLY
+var time = moment().format('LT');
+console.log(time);
 
-console.log(currentDay)
+
+var toDoArr = []
+
+
+$('.saveBtn').click(function(){
+  
+    var userInput = $('#input').val();
+    toDoArr.push(userInput);
+
+
+
+    localStorage.setItem('UserInput', JSON.stringify(toDoArr));
+    
+    
+    console.log(toDoArr);
+
+   
+});
+    $('#input').val(JSON.parse(localStorage.getItem('UserInput')));
+
+});
+
